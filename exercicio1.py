60665af94112d458229a18c8818bb8962eedebf7
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2024-11-07 09:21:04 -0400

    5

--- exercicio1.py
+++ exercicio1.py
@@ -4,7 +4,7 @@ aposta1 = int(input('Digite o primeiro número da sua aposta (0 a 9): '))
 aposta2 = int(input('Digite o segundo número da sua aposta (0 a 9): '))
 
 if aposta1 < 0 or aposta1 > 9 or aposta2 < 0 or aposta2 > 9:
-    print('Os números apostados devem estar entre 0 e 9')
+    print('Os números apostados devem estar entre os numeros 0 e 9')
 else:
     numeros_sorteados = [random.randint(0, 9) for _ in range(4)]
     print('Números sorteados:', numeros_sorteados)
